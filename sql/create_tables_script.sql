6588e267293862b0f138b4da2fc475f3357bad4e
Carlos Eduardo Oliveira Alves <carlos.eduardo.oliver.alves@gmail.com>
2024-10-22 15:50:36 -0300

    Uptade Create Tables Script

--- sql/create_tables_script.sql
+++ sql/create_tables_script.sql
@@ -1,24 +1,28 @@
-CREATE TABLE Medico (
-    id INTEGER PRIMARY KEY,
-    nome TEXT NOT NULL,
-    especialidade TEXT NOT NULL,
-    telefone TEXT
-);
 
-CREATE TABLE Paciente (
-    id INTEGER PRIMARY KEY,
-    nome TEXT NOT NULL,
-    data_nascimento DATE,
-    telefone TEXT
-);
+    USE consultas_medicas;
 
-CREATE TABLE Consulta (
-    id INTEGER PRIMARY KEY,
-    medico_id INTEGER,
-    paciente_id INTEGER,
-    data_consulta DATE,
-    hora_consulta TEXT,
-    status TEXT,
-    FOREIGN KEY (medico_id) REFERENCES Medico (id),
-    FOREIGN KEY (paciente_id) REFERENCES Paciente (id)
-);
+    CREATE TABLE Medico (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+        especialidade VARCHAR(255) NOT NULL,
+        telefone VARCHAR(20)
+    );
+    
+    CREATE TABLE Paciente (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+        data_nascimento DATE,
+        telefone VARCHAR(20)
+    );
+    
+    CREATE TABLE Consulta (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        medico_id INT,
+        paciente_id INT,
+        data_consulta DATE,
+        hora_consulta TIME,
+        status VARCHAR(50),
+        FOREIGN KEY (medico_id) REFERENCES Medico (id),
+        FOREIGN KEY (paciente_id) REFERENCES Paciente (id)
+    );
+
